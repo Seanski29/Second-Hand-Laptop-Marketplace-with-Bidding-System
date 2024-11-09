f4edb68cdf8175eb98b873682d83b20687e05a86
Seanski29 <seanmdelrosario@gmail.com>
2024-11-09 22:16:37 +0800

    second or third commit

--- index.php
+++ index.php
@@ -11,7 +11,9 @@
     <!-----------------------------START OF NAVBAR------------------------------->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <img src="assets,images,Logo.jpg"/>
+    <img src="assets/images/Logo.webp"
+    width="45" 
+    height="55" />
     <a class="navbar-brand" href="#">LaptopHaven</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -33,10 +35,6 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Contact</a>
         </li>
-        <li class="nav-item">
-        <i class="fa-solid fa-basket-shopping"></i>
-        </div>
-        </li>
        
 </div>
     <form class="d-flex" role="search">
@@ -45,7 +43,39 @@
     </form>
 </nav>   
     <!-----------------------------END OF NAVBAR-------------------------------->
+    <!-----------------------------Start of HOME text-------------------------------->
+    <section id="home">
+      <div class="container">
+        <h5>WELCOME TO LAPTOPHAVEN</h5>
+        <h1>Find your Second Hand Choices Here</h1>
+        <p>We offer the best prices and auctions</p>
+        <button>Find Now</button>
+      </div>
+    </section>
+    <!-----------------------------End of HOME text-------------------------------->
+    <!-----------------------------Start of BRANDS text-------------------------------->
+    <h3>Brands</h3>
+    <section id="brand" class="containter">
+      <div class="row">
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Asus.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/MSI.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Lenovo.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Gigabyte.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Dell.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/HP.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Acer.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Razer.jpeg"/>
+        <img class="img-fluid col-lg-3 col-md-6 col-sm-12" src="assets/imgages/Others.jpeg"/>
+      </div>
+  </section>
+  <h3>Deals</h3>
+    <section id="deals" class="w-100">
+      <div class="row p-0 m-0">
+        <div class="one col-lg-4 col-md-12 col-sm-12 p-0">
+          <img class="img-fluid>
+        </div>
+      </div>
 
-
+    </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </html>
